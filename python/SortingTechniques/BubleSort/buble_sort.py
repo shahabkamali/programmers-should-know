f3b3e64d12cb4368319fad5af628fc139da6b779
shahab <shahab kamali>
2019-03-09 22:55:15 +0330

    add bubble sort in another way

--- python/SortingTechniques/BubleSort/buble_sort.py
+++ python/SortingTechniques/BubleSort/buble_sort.py
@@ -6,6 +6,17 @@ def bubbleSort(alist):
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
+
+
+def bubble_sort2(alist):
+    for i in range(0, len(alist)-1):
+        for j in range(0, len(alist) - i - 1):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+
 alist = [54,26,93,17,77,31,44,55,20]
-bubbleSort(alist)
+bubble_sort2(alist)
 print(alist)
